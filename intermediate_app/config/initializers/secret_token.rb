38af327e42d9cfd019d1d6e978a8c846dcc0a67b
alma <almarates@gmail.com>
2012-11-06 19:18:04 -0800

    added all things

---
+++ intermediate_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IntermediateApp::Application.config.secret_token = '1d2eacfc30c883445394147b09d06f36e811e25fb6b4af50f2385705eae684172241e8a5384a4c2388e82d46f04955dcbaf5171f45ccd366029f6fe0067f8124'
